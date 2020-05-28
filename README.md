e57217ac952369a93a0e5fcae024a7bc9fbe79d7
Link Dupont <link@sub-pop.net>
2020-05-28 07:03:41 -0400

    Fiddle with flags for configuring app settings

--- README.md
+++ README.md
@@ -25,8 +25,12 @@ Configuration is done through environment variables.
 * `ADDR`: Address on which the HTTP server should listen (default: ":8080")
 * `MADDR`: Address on which the metrics HTTP server should listen (default:
    ":2112")
-* `ENV`: Mode in which to operate. One of: "production" or "development"
-   (default: "development")
-* `DB_PATH`: Path to a SQLite database (default: ":memory:")
-* `DB_DATA`: A CSV formatted string of module-name,account-id pairs with
-   which to seed the database (default: "")
+* `LOG_FORMAT`: Format of log output (either "json" or "text") (default: "text")
+* `DB_DRIVER`: Database driver to use (either "pgx" or "sqlite3")
+   (default: "sqlite3")
+* `DATABASE_URL`: A URL forming a database connection string (i.e. "file::memory:")
+* `DB_HOST`: Address of the database server (default: "localhost")
+* `DB_PORT`: TCP port of the database server (default: "5432")
+* `DB_NAME`: Name of the database (default: "postgres")
+* `DB_USER`: Username on the database server (default: "postgres")
+* `DB_PASS`: Password of the database user
--- main.go
+++ main.go
@@ -16,42 +16,52 @@ import (
 
 func main() {
 	var (
-		addr       string // addr is the TCP address and port the application listens on
-		maddr      string // maddr is the TCP address and port the metrics HTTP server listens on
-		logLevel   string // log level
-		env        string // env determines operation mode (log formatters, etc.)
-		dbdata     string // initial data to populate database
-		pathprefix string // API path prefix
-		appname    string // API path app name
-		dbHost     string // IP or hostname of database server
-		dbPort     string // TCP port on database server
-		dbName     string // database name
-		dbUser     string // database username
-		dbPass     string // database user password
+		addr       string
+		maddr      string
+		logLevel   string
+		logFormat  string
+		pathprefix string
+		appname    string
+		dbDriver   string
+		dbHost     string
+		dbPort     string
+		dbName     string
+		dbUser     string
+		dbPass     string
+		dbURL      string
+		migrate    bool
+		seedpath   string
 	)
 
 	const (
 		apiversion = "v1"
 	)
 
-	fs := flag.NewFlagSet("module-update-router", flag.ExitOnError)
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	fs.StringVar(&addr, "addr", ":8080", "app listen address")
 	fs.StringVar(&maddr, "maddr", ":2112", "metrics listen address")
-	fs.StringVar(&logLevel, "log-level", "info", "default logging level")
-	fs.StringVar(&env, "environment", "development", "operation mode")
+	fs.StringVar(&logLevel, "log-level", "info", "logging level")
+	fs.StringVar(&logFormat, "log-format", "text", "set logging format (choice of 'json' or 'text')")
 	fs.StringVar(&pathprefix, "path-prefix", "/api", "API path prefix")
 	fs.StringVar(&appname, "app-name", "", "name component for the API prefix")
-	fs.StringVar(&dbdata, "db-data", "", "initial database seed data")
-	fs.StringVar(&dbHost, "db-host", "localhost", "IP or hostname of database")
+	fs.StringVar(&dbDriver, "db-driver", "sqlite3", "database driver ('pgx' or 'sqlite3')")
+	fs.StringVar(&dbHost, "db-host", "localhost", "IP or hostname of database server")
 	fs.StringVar(&dbPort, "db-port", "5432", "TCP port on database server")
 	fs.StringVar(&dbName, "db-name", "postgres", "database name")
 	fs.StringVar(&dbUser, "db-user", "postgres", "database username")
 	fs.StringVar(&dbPass, "db-pass", "", "database user password")
+	fs.StringVar(&dbURL, "database-url", "", "database connection URL")
+	fs.BoolVar(&migrate, "migrate", false, "run migrations")
+	fs.StringVar(&seedpath, "seed-path", "", "path to the SQL seed file")
 
 	ff.Parse(fs, os.Args[1:], ff.WithEnvVarNoPrefix())
 
-	switch env {
-	case "production":
+	if dbURL == "" && (dbHost == "" || dbPort == "" || dbName == "" || dbUser == "") {
+		log.Fatal("error: unable to connect to database. See -help for details")
+	}
+
+	switch logFormat {
+	case "json":
 		log.SetFormatter(&log.JSONFormatter{})
 	default:
 		log.SetFormatter(&log.TextFormatter{})
@@ -63,35 +73,42 @@ func main() {
 	}
 	log.SetLevel(lvl)
 
-	var driver, connString string
-	switch env {
-	case "production":
-		driver = "pgx"
-		connString = fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-			dbUser, dbPass, dbHost, dbPort, dbName)
-	default:
-		driver = "sqlite3"
-		connString = "file::memory:?cache=shared"
-
-		if envDriver, ok := os.LookupEnv("DB_DRIVER"); ok {
-			driver = envDriver
-			connString = dbName
+	var connString string
+	switch dbDriver {
+	case "pgx":
+		if dbURL != "" {
+			connString = dbURL
+		} else {
+			connString = fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+				dbUser, dbPass, dbHost, dbPort, dbName)
 		}
+	case "sqlite3":
+		if dbURL != "" {
+			connString = dbURL
+		} else {
+			connString = "file::memory:?cache=shared"
+		}
+	default:
+		log.Fatalf("error: unsupported database: %v", dbDriver)
 	}
 
-	db, err := Open(driver, connString)
+	db, err := Open(dbDriver, connString)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
-	if err := db.Migrate(); err != nil {
-		log.Fatal(err)
-	}
-
-	if len(dbdata) > 0 {
-		if err := db.Load(dbdata); err != nil {
+	if migrate {
+		if err := db.Migrate(); err != nil {
 			log.Fatal(err)
 		}
+
+		if seedpath != "" {
+			if err := db.Seed(seedpath); err != nil {
+				log.Fatal(err)
+			}
+		}
+		os.Exit(0)
 	}
 
 	srv, err := NewServer(addr, path.Join(pathprefix, appname, apiversion), db)
