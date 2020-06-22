fd75df479ef4c7af1154be1438e9c1229aeb517b
Link Dupont <link@sub-pop.net>
2020-06-22 13:00:56 -0400

    errors: build JSONError using a map instead

--- error.go
+++ error.go
@@ -11,27 +11,14 @@ import (
 // formatError converts a basic HTTP status code and message into a JSON API
 // error object, serializes it to JSON and writes it to w.
 func formatJSONError(w http.ResponseWriter, code int, msg string) {
-	type response struct {
-		Errors []struct {
-			Status string `json:"status"`
-			Title  string `json:"title"`
-		} `json:"errors"`
+	r := map[string]interface{}{
+		"errors": []map[string]interface{}{
+			{
+				"status": http.StatusText(code),
+				"title":  msg,
+			},
+		},
 	}
-
-	r := response{
-		Errors: []struct {
-			Status string "json:\"status\""
-			Title  string "json:\"title\""
-		}{},
-	}
-
-	r.Errors = append(r.Errors, struct {
-		Status string "json:\"status\""
-		Title  string "json:\"title\""
-	}{
-		Status: http.StatusText(code),
-		Title:  msg,
-	})
 
 	data, err := json.Marshal(&r)
 	if err != nil {
