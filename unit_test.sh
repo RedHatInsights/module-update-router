28bffb7866de1627486ae5e3334a126f79b04e11
Link Dupont <link@sub-pop.net>
2022-08-24 15:19:18 -0400

    ci: add go-junit-report to unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -2,4 +2,6 @@
 
 set -xev
 
-go test -v -race
+mkdir -p artifacts
+go install github.com/jstemmer/go-junit-report/v2@latest
+go test -v -race 2>&1 | ~/go/bin/go-junit-report -set-exit-code > artifacts/junit-report.xml
